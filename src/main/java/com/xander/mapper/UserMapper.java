c8f692523e581b22eb578a61f8861e97aff0e8d4
zhaobing04 <zhaobing04@58ganji.com>
2019-09-17 15:17:37 +0800

    修改注释

--- src/main/java/com/xander/mapper/UserMapper.java
+++ src/main/java/com/xander/mapper/UserMapper.java
@@ -2,12 +2,14 @@ package com.xander.mapper;
 
 import com.xander.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by zhaobing04 on 2019/9/16.
  */
+@Repository
 @Mapper
 public interface UserMapper {
     //获取用户名单
