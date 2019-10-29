3c20e1162f32953ab0ae2668181c7b1b68f9f7ed
zhaobing04 <zhaobing04@58ganji.com>
2019-10-29 14:35:52 +0800

    重构原型模式

--- src/test/java/com/xander/designpattern/createtype/prototype/CircleShenClone.java
+++ src/test/java/com/xander/designpattern/createtype/prototype/CircleShenClone.java
@@ -1,7 +1,5 @@
 package com.xander.designpattern.createtype.prototype;
 
-import com.alibaba.fastjson.JSON;
-
 /**
  * Created by zhaobing04 on 2019/10/17.
  */
@@ -40,19 +38,5 @@ public class CircleShenClone implements Cloneable{
         }
         circleShenClone.setDot((Dot) dot.clone());
         return circleShenClone;
-    }
-
-
-    public static void main(String[] args) {
-        CircleShenClone circle = new CircleShenClone();
-        circle.setDot(new Dot(0,0));
-        System.out.println(" circle dot:" + JSON.toJSONString(circle));
-
-        CircleShenClone circle1 = (CircleShenClone) circle.clone();
-        circle1.getDot().setX(2);
-        circle1.setR(2);
-
-        System.out.println(" circle dot:" + JSON.toJSONString(circle));
-        System.out.println(" circle1 dot:" + JSON.toJSONString(circle1));
     }
 }
